144bbb3c2ce36876d8e9fdb8ea51919e26e912d2
Artem Lubenskiy <aalubenskiy@gmail.com>
2014-04-02 17:14:38 +0300

    Create authorization

--- module/Users/src/Users/Controller/LoginController.php
+++ module/Users/src/Users/Controller/LoginController.php
@@ -10,10 +10,15 @@ namespace Users\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Authentication\AuthenticationService;
+use Zend\Authentication\Adapter\DbTable as DbTableAuthAdapter;
 use Users\Form\LoginForm;
 use Users\Form\LoginFilter;
 
 class LoginController extends AbstractActionController {
+    
+    protected $_authservice;
+    
     public function indexAction() {
         $form = new LoginForm();
         $viewModel = new ViewModel(array('form' => $form));
@@ -40,10 +45,32 @@ class LoginController extends AbstractActionController {
             $viewModel->setTemplate('users/login/index');
             return $viewModel;
         }
-        
-        return $this->redirect()->toRoute(NULL, array(
-                    'controller' => 'index',
-                    'action' => 'index'
+        $this->getAuthService()->getAdapter()
+                ->setIdentity($this->request->getPost('email'))
+                ->setCredential($this->request->getPost('password'));
+        $result = $this->getAuthService()->authenticate();
+        if($result->isValid()){
+            $this->getAuthService()->getStorage()->write($this->request->getPost('email'));
+            return $this->redirect()->toRoute(NULL, array(
+                    'controller' => 'login',
+                    'action' => 'confirm'
+                    ));
+        }
+    }
+    public function getAuthService() {
+        if (!$this->_authservice) {
+            $dbAdapter = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
+            $dbTableAuthAdapter = new DbTableAuthAdapter($dbAdapter, 'users', 'email', 'password', 'MD5(?)');
+            $this->_authservice = new AuthenticationService();
+            $this->_authservice->setAdapter($dbTableAuthAdapter);
+        }
+        return $this->_authservice;
+    }
+    public function confirmAction() {
+        $user_email = $this->getAuthService()->getStorage()->read();
+        $viewModel = new ViewModel(array(
+            'user_email' => $user_email
         ));
+        return $viewModel;
     }
 }
